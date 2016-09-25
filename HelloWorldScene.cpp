ee6f47b4a29939f3830c8d26ec7afdb97ca07fc9
Valentin <kozlov.valent@gmail.com>
2016-09-26 02:11:29 +0400

    Encore : Police Animatiom SpriteSheet Probem at xAce

--- HelloWorldScene.cpp
+++ HelloWorldScene.cpp
@@ -355,10 +355,10 @@ void HelloWorld::logSpeedLastLine(float dt)
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
     //Close the cocos2d-x game scene and quit the application
-    Director::getInstance()->end();
+ //   Director::getInstance()->end();
 
     #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
+ //   exit(0);
 #endif
     
     /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/

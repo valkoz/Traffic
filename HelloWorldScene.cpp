0819d1b8f3e0a65900fefebd6aad76f0f4306f8f
Valentin <kozlov.valent@gmail.com>
2016-09-29 00:17:18 +0400

    version 0.1 / full of bags but funny!

--- HelloWorldScene.cpp
+++ HelloWorldScene.cpp
@@ -1,6 +1,7 @@
 #include "HelloWorldScene.h"
 #include "SimpleAudioEngine.h"
 #include "Definitions.h"
+#include <vector>
 //#include <iostream>
 
 USING_NS_CC;
@@ -25,18 +26,7 @@ bool HelloWorld::init()
 
 	Device::setAccelerometerEnabled(true);
 	auto listener = EventListenerAcceleration::create(CC_CALLBACK_2(HelloWorld::onAcceleration, this));
-
 	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
-
-    auto closeItem = MenuItemImage::create("CloseNormal.png", "CloseSelected.png", CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
-    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
 
 
 	auto touchListener = EventListenerTouchOneByOne::create();
@@ -44,8 +34,6 @@ bool HelloWorld::init()
 	touchListener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
 	touchListener->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchEnded, this);
 	touchListener->onTouchMoved = CC_CALLBACK_2(HelloWorld::onTouchMoved, this);
-
-
 	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(touchListener, this);
 
 //	this->scheduleUpdate();
@@ -62,80 +50,84 @@ bool HelloWorld::init()
 	road_upper_part->setPosition(Vec2(visibleSize.width / 2 + origin.x, 3 * visibleSize.height / 2 + origin.y));
 	this->addChild(road_upper_part);
 
-	user_car = Sprite::create("cars/car_red.png");
+	user_car = Sprite::create("cars/Mycar.png");
 	user_car->setPosition(Vec2(visibleSize.width - LEFT_CAR_POSITION, 150));
+
 	user_car->setName("user");
-	user_car->setRotation(-90);
 	this->addChild(user_car,1);
 
 	this->schedule(schedule_selector(HelloWorld::generateNewCar), 1.0f);
 
 	this->scheduleUpdate();
-	//user_car->runAction(seq);
+	
+for (int i = 0; i < 50; i++)
+{    
+	accInfo.push_back(0);
+}
 
-    // add the sprite as a child to this layer
-   // this->addChild(sprite, 0);
-    
     return true;
 }
 
 void HelloWorld::onAcceleration(cocos2d::Acceleration *acc, cocos2d::Event *event)
 {
-	accInfo = acc->x;
-//	auto user_car = Sprite::create("car_red.png");
-//	user_car->setPosition(0,0);
-//	this->addChild(user_car, 1);
-//	CCLOG("%f", acc->z);
+	accInfo.push_back(acc->x);
+	if (accInfo.size() > 50)
+		accInfo.erase(accInfo.begin() + 1);
+	CCLOG("%f", acc->x);
 }
 
-/*
-onTouchBegan works only one time but must work permanent for acceleration wtf?!
-can be used without shedulers but how?
-!!!!!!!!!!!!!!!!!!!!!!!!FIXED!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-Black strips on background
-
-*/
 bool HelloWorld::onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *event) {
 	Point touchPoint(touch->getLocation().x, touch->getLocation().y);
 	Rect gasRect(0, 0, 200, 200);
-	Rect stopRect(800, 0, 200, 200);
+	Rect stopRect(600, 0, 200, 200);
 	if (gasRect.containsPoint(touchPoint)) {
-//		CCLOG("GOOOOO");
 		onTouch = gas;
 	}
 	if (stopRect.containsPoint(touchPoint))
 		onTouch = brakes;
-//	this->schedule(schedule_selector(HelloWorld::increaseSpeed), 0.1f);
 	return true;
 }
 
 void HelloWorld::onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event) {
 	onTouch = freeMovement;
-//	this->schedule(schedule_selector(HelloWorld::decreaseSpeed), 0.1f);
 	return;
 }
 
 void HelloWorld::onTouchMoved(cocos2d::Touch *touch, cocos2d::Event *event) {
 }
 
-void HelloWorld::increaseSpeed(float dt) {
-	carSpeed += 1.1f;
-}
-
-void HelloWorld::decreaseSpeed(float dt) {
-	carSpeed -= 1.1f;
-}
-
+/*Game loop*/
 void HelloWorld::update(float dt) {
-	/*нопедекъер онбедемхе юбрн 1 - цюг, -1 - рнплнг, 0 - ябнандмне дбхфемхе*/
-	if ((onTouch == gas) && (carSpeed < 200))
-		carSpeed += 1;
-	else if ((onTouch == brakes) && (carSpeed > MIN_SPEED))
-		carSpeed -= 3;
-	else if ((onTouch == freeMovement) && (carSpeed > MIN_SPEED))
-		carSpeed -= 1;
-	/*опнпхянбйю тнмю*/
+	this->changeSpeed();
+	this->renderBackground();
+	
+
+	/*user_car with accelerometer*/
+	if ((user_car->getPositionX()  + (user_car->getBoundingBox().getMaxX() - user_car->getBoundingBox().getMinX())/2 > 720) || (user_car->getPositionX() - (user_car->getBoundingBox().getMaxX() - user_car->getBoundingBox().getMinX()) / 2 < 0)) { CCLOG("vibration"); }
+	else {
+		double res = this->returnAccRatio();
+		user_car->setRotation(2 * res);
+		user_car->setPosition(Vec2(user_car->getPositionX() + res, user_car->getPositionY()));
+	}
+
+
+	/*опнпхянбйю люьхм*/ /*+ хмреккейр люьхм (бнглнфмнярэ нанцмюрэ)*/
+	Vector<Node*> allNodes = this->getChildren();
+	for (auto& node : allNodes) {
+		if (dynamic_cast<Car*>(node)) {
+			Car *currentCar = dynamic_cast<Car*>(node);
+			this->removeCarFromLayer(currentCar);
+			this->checkCarsNearWithUpdate(currentCar);
+			this->checkCollisions(currentCar);
+			this->overtake(currentCar);
+		}
+	}	
+}
+
+/*Renderring the Background*/
+void HelloWorld::renderBackground()
+{
 	auto visibleSize = Director::getInstance()->getVisibleSize();
 	auto origin = Director::getInstance()->getVisibleOrigin();
 	road->setPosition(Vec2(visibleSize.width / 2 + origin.x, road->getPositionY() - 0.1 * carSpeed));
@@ -144,39 +136,111 @@ void HelloWorld::update(float dt) {
 		road->setPosition(Vec2(visibleSize.width / 2 + origin.x, 3 * visibleSize.height / 2 + origin.y));
 	if (road_upper_part->getPositionY() < -visibleSize.height / 2 - origin.y)
 		road_upper_part->setPosition(Vec2(visibleSize.width / 2 + origin.x, 3 * visibleSize.height / 2 + origin.y));
-
-/*user_car with accelerometer*/
-user_car->setPosition(Vec2(user_car->getPositionX() + 10 * accInfo, user_car->getPositionY()));
-
-	/*опнпхянбйю люьхм*/
-	Vector<Node*> allNodes = this->getChildren();
-	for (auto& node : allNodes) {
-		if (dynamic_cast<Car*>(node)) {
-			Car *target = dynamic_cast<Car*>(node);
-			target->setPosition(Vec2(target->getPositionX() , target->getPositionY() - 0.1 * (carSpeed - target->getSpeed())));
-		}
-	}
 }
 
+/* Generate new Car and set it on layer*/
 void HelloWorld::generateNewCar(float dt) {
 	auto nCar = Car::create();
 	this->addChild(nCar,100);
-	CCLOG("Current Speed: %i", carSpeed);
 }
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
+/*Removes Car *current from layer */
+void HelloWorld::removeCarFromLayer(Car * current)
 {
-    //Close the cocos2d-x game scene and quit the application
-    Director::getInstance()->end();
+	if (current->getPositionY() < -200)
+		this->removeChild(current, true);
+	if (current->getPositionY() > 3000)
+		this->removeChild(current, true);
+}
 
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
-    
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-    
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-    
-    
+
+/*Checks nerby cars and set moveOption variable for Car *current*/
+void HelloWorld::checkCarsNearWithUpdate(Car *current)
+{
+	MoveOption opt = MoveOption(false, false, false);
+	Vector <Node*> fuckingFronts = this->getChildren();
+	for (auto& car : fuckingFronts) {
+		if (dynamic_cast<Car*>(car)) {
+			Car *nearbyCar = dynamic_cast<Car*>(car);
+			if (current->getPosition() != nearbyCar->getPosition()) {
+				if ((current->getPositionX() == nearbyCar->getPositionX()) && (nearbyCar->getPositionY() - current->getPositionY() < 400) && (nearbyCar->getPositionY() - current->getPositionY() > 0)) {
+					if (nearbyCar->getSpeed() <= current->getSpeed())
+						opt.setIsFront(true);
+				}
+				if ((current->getPositionX() - nearbyCar->getPositionX() <= ROAD_LINE_WIDTH) && (current->getPositionX() - nearbyCar->getPositionX() > 0) && (abs(nearbyCar->getPositionY() - current->getPositionY()) < 600)) {
+					opt.setIsLeft(true);
+				}
+				if ((nearbyCar->getPositionX() - current->getPositionX() <= ROAD_LINE_WIDTH) && (nearbyCar->getPositionX() - current->getPositionX() > 0 ) && (abs(nearbyCar->getPositionY() - current->getPositionY()) < 600)) {
+					opt.setIsRight(true);
+				}
+			}
+		}
+		current->setMoveOption(opt.getIsFront(), opt.getIsLeft(), opt.getIsRight());
+	}
+}
+
+/*Collision Detection-> GAME OVER*/
+void HelloWorld::checkCollisions(Car * currentCar)
+{
+	Rect a = user_car->getBoundingBox();
+	float minX = a.getMinX() + 15;
+	float maxX = a.getMaxX() - 15;
+	float minY = a.getMinY();
+	float maxY = a.getMaxY();
+	Rect *newRectangle = new Rect(minX, minY, maxX - minX, maxY - minY - 5);
+
+
+	if (currentCar->getBoundingBox().intersectsRect(*newRectangle)) {
+		auto scene = HelloWorld::createScene();
+		Director::getInstance()->replaceScene(TransitionFade::create(1.0f, scene));
+	}
+}
+
+/*Checks if overtake is nessesry and making an overtake*/
+void HelloWorld::overtake(Car * currentCar)
+{
+	if (!currentCar->getActionByTag(1)) {
+		if (currentCar->getIsFront())
+		{
+			if (currentCar->getIsLeft()) {
+				if (currentCar->getIsRight())
+					currentCar->decreaseSpeed(carSpeed);
+				else {
+					if (currentCar->getPositionX() != LEFT_CAR_POSITION + 3 * ROAD_LINE_WIDTH)
+						currentCar->moveToRightLine(carSpeed);
+					else
+						currentCar->decreaseSpeed(carSpeed);
+				}
+			}
+			else {
+				if (currentCar->getPositionX() != LEFT_CAR_POSITION)
+					currentCar->moveToLeftLine(carSpeed);
+				else if (currentCar->getIsRight())
+					currentCar->decreaseSpeed(carSpeed);
+				else
+					currentCar->moveToRightLine(carSpeed);
+			}
+		}
+		else { currentCar->increaseSpeed(carSpeed); }
+	}
+}
+
+/*Returns value of accelerometer coefficient*/
+double HelloWorld::returnAccRatio()
+{
+	double res = 0;
+	for (auto i : accInfo) 
+		res = res + 0.3 * (i+1) * accInfo[49 - i];
+	return res;
+}
+
+/*Making player's car to move faster or slower */
+void HelloWorld::changeSpeed()
+{
+	if ((onTouch == gas) && (carSpeed < 200))
+		carSpeed += 1;
+	if ((onTouch == brakes) && (carSpeed > MIN_SPEED))
+		carSpeed -= 3;
+	if ((onTouch == freeMovement) && (carSpeed > MIN_SPEED))
+		carSpeed -= 1;
 }
--- HelloWorldScene.h
+++ HelloWorldScene.h
@@ -3,6 +3,7 @@
 
 #include "cocos2d.h"
 #include "Car.h"
+#include <vector>
 
 enum moveOption { brakes = -1, freeMovement, gas };
 
@@ -12,25 +13,35 @@ private:
 	cocos2d::Sprite *road;
 	cocos2d::Sprite *road_upper_part;
 	cocos2d::Sprite *user_car;
-	float accInfo = 1;
-//	Car* nCar;
+
+	std::vector <double> accInfo;
 
 	moveOption onTouch = freeMovement;
+
 	int carSpeed = MIN_SPEED;
+
 	void update(float dt);
 
-	void increaseSpeed(float dt);
-	void decreaseSpeed(float dt);
+	void renderBackground();
 
 	void generateNewCar(float dt);
+
+	void removeCarFromLayer(Car *current);
+
+	void checkCarsNearWithUpdate(Car *current);
+
+	void checkCollisions(Car *currentCar);
+
+	void overtake(Car *currentCar);
+
+	double returnAccRatio();
+
+	void changeSpeed();
 
 public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
 
 	void onAcceleration(cocos2d::Acceleration *acc, cocos2d::Event *event);
 
